dab9503a55e6911783029ef3466ca96d057b01f6
jmnum <jmbastide@gmail.com>
2021-01-09 10:29:53 +0100

    Update stats.py

--- numerox/stats.py
+++ numerox/stats.py
@@ -4,22 +4,40 @@ import numpy as np
 def spearmanr(target, pred):
     return np.corrcoef(target, pred.rank(pct=True, method="first") )[0, 1]
     
- def ar1(x):
+def ar1(x):
     return np.corrcoef(x.iloc[:-1], x.iloc[1:])[0,1]
     
-def score_correlation(labels, predictions):
-    if type(predictions) is not pd.core.frame.DataFrame:
-        predictions_df = pd.DataFrame(predictions)
-    else:
-        predictions_df = predictions
-    ranked_predictions = predictions_df.rank(pct=True, method='first')
-
-    return np.corrcoef(labels, np.array(ranked_predictions)[:, 0])[0, 1]
-    
-def era_scores(data):
-   new_df = data['validation'].df[['era','target']].copy()
-   scores = pd.Series(index=new_df["era"].unique(),dtype='float32')
+def correlation_per_era(data,ppname):
+   new_df=ppname.df
+   new_df.columns=['pred']
+   new_df=new_df.join(data.region_isin(['train','validation']).df[['era','target']],how='inner')
+   era_scores = pd.Series(index=new_df["era"].unique(),dtype='float32')
    for era in new_df["era"].unique():
       era_df = new_df[new_df["era"] == era]
-      scores[era] = spearmanr( era_df["target"],era_df["pred"])
-   era_scores.sort_values(inplace=True)
+      era_scores[era] = spearmanr( era_df["target"],era_df["pred"])
+  
+   return era_scores.to_frame(name='corr')
+
+def mean_correlation(data,ppname):
+    era_scores= correlation_per_era(data,ppname)['corr']
+    return era_scores.mean()
+
+def sharpe(data,ppname):
+    era_scores= correlation_per_era(data,ppname)['corr']
+    return era_scores.mean()/era_scores.std()
+    
+def autocorrelation(data,ppname):
+    era_scores= correlation_per_era(data,ppname)['corr']
+    return ar1(era_scores)
+
+def FeatExp(data,ppname):
+    new_df=ppname.df
+    new_df.columns=['pred']
+    new_df=new_df.join(data.region_isin(['train','validation']).df,how='inner')
+    correlations = pd.Series(dtype='float32')
+    for feature in new_df.filter(like='feature'):
+        feat_corr=np.corrcoef(new_df['pred'], new_df[feature])[0,1]
+        correlations[feature]=feat_corr
+  
+    return correlations.to_frame(name='corr')
+   
